634570959d4a94439cb08463425c39af9829d5c0
Clara <c_c_456@hotmail.com>
2017-11-18 11:40:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const names = ['Ada', 'Brendan', 'Ali']);
 
-function printBadges(name){
+function printBadges(names){
   for(i=0,i<printBadges,i++ ){
     console.log(`Welcome ${name[0]! You are employee ${i+1}}`);
   }

55fa135e95be7ca0b00a0e0bfbd4340dbfe9e548
Clara <c_c_456@hotmail.com>
2017-11-18 11:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function printBadges(names){
   for(let i=0; i < printBadges.length; i++ ){
     console.log(`Welcome ${names[0]}! You are employee ${i+1}`);
   }
-  return printBadges;
+  return names;
 }
 
 printBadges(names);
